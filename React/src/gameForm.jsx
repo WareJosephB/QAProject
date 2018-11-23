091d35f8fff0b1a0bf33a157a4939d4dfb29a725
WareJosephB <warejosephb@gmail.com>
2018-11-23 11:17:10 +0000

    Trying the update by game function

--- React/src/gameForm.jsx
+++ React/src/gameForm.jsx
@@ -7,10 +7,11 @@ import Axios from 'axios';
 export default class GameForm extends Component {
 
     changeELO = () => {
-        Axios.put(game+"/match", {
+        Axios.post(game+"/match", {
                 player1 : document.getElementById('player1').value,
                 player2 : document.getElementById('player2').value
             }).then(response => {
+                console.log(response);
                 document.getElementById('Confirm Player').innerHTML = response.data.message;
             }).catch(function (error) {
                 console.log(error)
