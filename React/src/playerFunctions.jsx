162c3a8a1ac56ec0125ab41964504a9a9c051367
WareJosephB <ware.josephb@gmail.com>
2018-11-25 14:08:02 +0000

    Apparently I removed getPlayer for some reason (probably a good one), but the front end doesn't work without it... I could have sworn this was all working when I left work on Friday

--- React/src/playerFunctions.jsx
+++ React/src/playerFunctions.jsx
@@ -1,6 +1,12 @@
 import Axios from 'axios';
 import {player, add, get, upd, del} from './const.jsx';
 
+export function getPlayer(playerNumber){
+    Axios.get(player+get+playerNumber).then(response => {
+        return(response.data);
+    })
+}
+
 export const addPlayer = (event) => {
     event.preventDefault();
     Axios.post(player+add, {
